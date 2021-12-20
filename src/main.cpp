9040b753f50390b819df6fffb0c0cb734eff9c23
victor <victormolinaqui@gmail.com>
2021-12-20 12:19:32 +0100

    Hola

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1, n2, total;
+
+    cout << "Escribe un numero: " << endl;
+
+    cin >> n1;
+
+    cout << "Escribe un numero: " << endl;
+
+    cin >> n2;
+
+    total=n1+n2;
+
+    cout << endl;
+
+    cout << "El resultado es " << total;
+
+    return 0;
+}
